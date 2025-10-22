50506d6221aea059e8c67d47f07cad44f7befd98
kamalesh <sudhakarkamalesh7@gmail.com>
2025-10-22 21:24:16 +0530

    storyView Commit

---
+++ client/src/pages/Feed.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import StoriesBar from '../components/StoriesBar'
+
+const Feed = () => {
+
+  const [feeds, setfeeds] = useState([])
+  const [loading, setloading] = useState(true)
+
+  const fetchFeeds =async () => {
+    setfeeds(dummyPostsData)
+    setloading(false)
+  }
+
+  useEffect(()=> {
+    fetchFeeds()
+  },[])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+        {/* Stories and post list */}
+        <div>
+          <StoriesBar/>
+        <div className='p-4 space-y-6'>
+            List of post
+        </div>
+        </div>
+        {/* Right Sidebar */}
+        <div>
+          <div>
+            <h1>Sponsored</h1>
+          </div>
+          <h1>Recent messages</h1>
+        </div>
+    </div>
+  ) : (
+    <Loading/>
+  )
+  
+}
+
+export default Feed
