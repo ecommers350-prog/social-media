06e34a6d24bb8220cb4195000f595084d7f82e74
kamalesh <sudhakarkamalesh7@gmail.com>
2025-10-25 22:35:26 +0530

    server update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,17 +8,25 @@ import Connection from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
-import {useUser} from '@clerk/clerk-react'
+import {useUser, useAuth} from '@clerk/clerk-react'
 import Layout from './pages/Layout'
 import {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react'
 
 const App = () => {
   const {user} = useUser()
+    const {getToken} = useAuth()
+
+  useEffect(()=>{
+    if(user){
+      getToken().then((token)=>console.log(token))
+    }
+  },[user])
   return (
     <>
       <Toaster/>
       <Routes>
-        <Route path='/'element={ !user ? <Login /> : <Layout/>}>
+        <Route path='/' element={ !user ? <Login /> : <Layout/> }>
           <Route index element={<Feed />}/>
           <Route path='messages' element={<Messages />}/>
           <Route path='messages/:userId' element={<ChatBox />}/>
